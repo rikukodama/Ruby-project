14b5aad60db3ba00e1aa6d7b8d5b856a547f2f81
CrystalStar0920 <crystalstar0920@gmail.com>
2024-03-19 22:44:35 +0900

    initial

--- spec/bar_spec.rb
+++ spec/bar_spec.rb
@@ -1,9 +1,13 @@
 require 'spec_helper'
 
 describe Bar do
+
   describe '#print_fine' do
+
     context 'sunny path' do
+
       it 'always print hello world' do
+
         expect(subject.print_fine).to eq('This is fine :)')
       end
     end
