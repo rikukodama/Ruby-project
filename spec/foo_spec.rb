54385da472f84905aea1c5f505f4f769c97bf4b5
CrystalStar0920 <crystalstar0920@gmail.com>
2024-03-24 22:00:38 +0900

    crystal

--- spec/foo_spec.rb
+++ spec/foo_spec.rb
@@ -1,12 +1,19 @@
 require 'spec_helper'
 
 describe Foo do
+
   describe '#hello_world' do
+
     context 'sunny path' do
+
       it 'always print hello world' do
+
         expect(subject.print_hello).to eq('Hello world!')
+
       end
+
     end
+
   end
 
   describe '#print_nothing' do
